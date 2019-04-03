d18d77cb856d90e7a72bb7ffa0997c1e9f056e69
jiangyuwise <jiangyuwise@gmail.com>
2019-04-03 17:46:05 +0800

    use lombok dependency to reduce the coding work of getter and setter and constructor.

--- src/main/java/com/codve/PageVisit.java
+++ src/main/java/com/codve/PageVisit.java
@@ -1,53 +1,16 @@
 package com.codve;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.net.InetAddress;
 
 //用于存储用户的访问记录
+@Data
 public class PageVisit implements Serializable {
     private long enteredTimestamp; // 访问进入时间戳
     private Long leftTimestamp; // 访问离开时间戳
     private String request; // 请求字符串
     private InetAddress ipAddress; //  ip地址
-
-    public long getEnteredTimestamp()
-    {
-        return enteredTimestamp;
-    }
-
-    public void setEnteredTimestamp(long enteredTimestamp)
-    {
-        this.enteredTimestamp = enteredTimestamp;
-    }
-
-    public Long getLeftTimestamp()
-    {
-        return leftTimestamp;
-    }
-
-    public void setLeftTimestamp(Long leftTimestamp)
-    {
-        this.leftTimestamp = leftTimestamp;
-    }
-
-    public String getRequest()
-    {
-        return request;
-    }
-
-    public void setRequest(String request)
-    {
-        this.request = request;
-    }
-
-    public InetAddress getIpAddress()
-    {
-        return ipAddress;
-    }
-
-    public void setIpAddress(InetAddress ipAddress)
-    {
-        this.ipAddress = ipAddress;
-    }
 
 }
